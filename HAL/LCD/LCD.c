97c1182a73310b7aad778e77c8b3246d12cf4a9e
SotirMilad <sotirmilad@gmail.com>
2024-05-01 18:05:36 +0300

    implementation of lcd initialization and send command functions

--- HAL/LCD/LCD.c
+++ HAL/LCD/LCD.c
@@ -9,7 +9,7 @@ void LCD_Init(void){
 	switch(LCD_MODE){
 
 	  GPIO_Init(LCD_DATA_PORT);
-    GPIO_Init(LCD_CONTROL_PORT);
+      GPIO_Init(LCD_CONTROL_PORT);
 			
 		case LCD_8_BIT_MODE:
 			
@@ -25,13 +25,13 @@ void LCD_Init(void){
 			GPIO_SetPinDirection(LCD_DATA_PORT, LCD_EN_PIN, GPIO_OUTPUT);
 			
 			STK_Delay(1);
-      LCD_SendCommand(LCD_8_BIT_MODE_COMMAND);
-      LCD_SendCommand(LCD_CLEAR_COMMAND);
-      LCD_SendCommand(LCD_DISPLAY_ON_COMMAND);
-      break;  		
+            LCD_SendCommand(LCD_8_BIT_MODE_COMMAND);
+            LCD_SendCommand(LCD_CLEAR_COMMAND);
+            LCD_SendCommand(LCD_DISPLAY_ON_CURSOR_BLINKING);
+            break;  		
 			
 		
-		case LCD_4_BIT_MODE:
+		    case LCD_4_BIT_MODE:
 			
 			GPIO_SetPinDirection(LCD_DATA_PORT, LCD_D4_PIN, GPIO_OUTPUT);
 			GPIO_SetPinDirection(LCD_DATA_PORT, LCD_D5_PIN, GPIO_OUTPUT);
@@ -40,11 +40,11 @@ void LCD_Init(void){
 			GPIO_SetPinDirection(LCD_DATA_PORT, LCD_RS_PIN, GPIO_OUTPUT);
 			GPIO_SetPinDirection(LCD_DATA_PORT, LCD_EN_PIN, GPIO_OUTPUT);
 		  
-      STK_Delay(1);
-      LCD_SendCommand(LCD_4_BIT_MODE_COMMAND);
-      LCD_SendCommand(LCD_CLEAR_COMMAND);
-      LCD_SendCommand(LCD_DISPLAY_ON_COMMAND);		
-		  break;
+            STK_Delay(1);
+            LCD_SendCommand(LCD_4_BIT_MODE_COMMAND);
+            LCD_SendCommand(LCD_CLEAR_COMMAND);
+            LCD_SendCommand(LCD_DISPLAY_ON_CURSOR_BLINKING);		
+		    break;
 		
 	}
 
@@ -55,7 +55,7 @@ static void send_pulse(void){
 	GPIO_SetPinValue(LCD_CONTROL_PORT, LCD_EN_PIN, GPIO_HIGH);
 	STK_Delay(2);
 	GPIO_SetPinValue(LCD_CONTROL_PORT, LCD_EN_PIN, GPIO_LOW);
-  STK_Delay(2);
+    STK_Delay(2);
 
 }
 
@@ -71,33 +71,33 @@ void LCD_SendCommand(uint8_t Command){
 		
 		case LCD_8_BIT_MODE:
 			GPIO_SetPinValue(LCD_DATA_PORT, LCD_D0_PIN, Command >> 0 & 0x01);
-      GPIO_SetPinValue(LCD_DATA_PORT, LCD_D1_PIN, Command >> 1 & 0x01);
-      GPIO_SetPinValue(LCD_DATA_PORT, LCD_D2_PIN, Command >> 2 & 0x01);
-      GPIO_SetPinValue(LCD_DATA_PORT, LCD_D3_PIN, Command >> 3 & 0x01);
-      GPIO_SetPinValue(LCD_DATA_PORT, LCD_D4_PIN, Command >> 4 & 0x01);
-      GPIO_SetPinValue(LCD_DATA_PORT, LCD_D5_PIN, Command >> 5 & 0x01);
-      GPIO_SetPinValue(LCD_DATA_PORT, LCD_D6_PIN, Command >> 6 & 0x01);
-      GPIO_SetPinValue(LCD_DATA_PORT, LCD_D7_PIN, Command >> 7 & 0x01);
-		  send_pulse();
+            GPIO_SetPinValue(LCD_DATA_PORT, LCD_D1_PIN, Command >> 1 & 0x01);
+            GPIO_SetPinValue(LCD_DATA_PORT, LCD_D2_PIN, Command >> 2 & 0x01);
+            GPIO_SetPinValue(LCD_DATA_PORT, LCD_D3_PIN, Command >> 3 & 0x01);
+            GPIO_SetPinValue(LCD_DATA_PORT, LCD_D4_PIN, Command >> 4 & 0x01);
+            GPIO_SetPinValue(LCD_DATA_PORT, LCD_D5_PIN, Command >> 5 & 0x01);
+            GPIO_SetPinValue(LCD_DATA_PORT, LCD_D6_PIN, Command >> 6 & 0x01);
+            GPIO_SetPinValue(LCD_DATA_PORT, LCD_D7_PIN, Command >> 7 & 0x01);
+		    send_pulse();
 		  
-		  break;
+		    break;
 		
 		case LCD_4_BIT_MODE:
 			GPIO_SetPinValue(LCD_DATA_PORT, LCD_D4_PIN, Command >> 4 & 0x01);
-      GPIO_SetPinValue(LCD_DATA_PORT, LCD_D5_PIN, Command >> 5 & 0x01);
-      GPIO_SetPinValue(LCD_DATA_PORT, LCD_D6_PIN, Command >> 6 & 0x01);
-      GPIO_SetPinValue(LCD_DATA_PORT, LCD_D7_PIN, Command >> 7 & 0x01);
-		  send_pulse();
+            GPIO_SetPinValue(LCD_DATA_PORT, LCD_D5_PIN, Command >> 5 & 0x01);
+            GPIO_SetPinValue(LCD_DATA_PORT, LCD_D6_PIN, Command >> 6 & 0x01);
+            GPIO_SetPinValue(LCD_DATA_PORT, LCD_D7_PIN, Command >> 7 & 0x01);
+	        send_pulse();
 		
-		  command = command <<4;
+		    command = command <<4;
 		
-		  GPIO_SetPinValue(LCD_DATA_PORT, LCD_D4_PIN, Command >> 4 & 0x01);
-      GPIO_SetPinValue(LCD_DATA_PORT, LCD_D5_PIN, Command >> 5 & 0x01);
-      GPIO_SetPinValue(LCD_DATA_PORT, LCD_D6_PIN, Command >> 6 & 0x01);
-      GPIO_SetPinValue(LCD_DATA_PORT, LCD_D7_PIN, Command >> 7 & 0x01);
-		  send_pulse();
+		   GPIO_SetPinValue(LCD_DATA_PORT, LCD_D4_PIN, Command >> 4 & 0x01);
+           GPIO_SetPinValue(LCD_DATA_PORT, LCD_D5_PIN, Command >> 5 & 0x01);
+           GPIO_SetPinValue(LCD_DATA_PORT, LCD_D6_PIN, Command >> 6 & 0x01);
+           GPIO_SetPinValue(LCD_DATA_PORT, LCD_D7_PIN, Command >> 7 & 0x01);
+		   send_pulse();
 		  
-	   	break;
+	   	   break;
 	
 
 	}
